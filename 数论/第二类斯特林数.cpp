a0992c7726cf061d224c25b03962c9d0b2cad8ee
Ryan1702 <81510326+Ryan1702@users.noreply.github.com>
2021-08-07 15:48:44 +0800

    Create 第二类斯特林数.cpp

---
+++ 数论/第二类斯特林数.cpp
@@ -0,0 +1,11 @@
+void init(int n)
+{
+    dp[0][0]=dp[1][1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            dp[i][j]=dp[i-1][j]*j+dp[i-1][j-1];
+        }
+    }
+}
